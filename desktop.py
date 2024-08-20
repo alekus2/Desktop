9680dd077f531a7f79458510a35b249e3c537735
alekus2 <alekviniciuscorreia@gmail.com>
2024-08-20 07:49:16 -0400

    -atividade: Crie um código no qual voce tera um botao na tela que fará a impressao em console de quantas vezes o botao foi clicado

--- desktop.py
+++ desktop.py
@@ -3,13 +3,16 @@ from tkinter import ttk
 def contar_cliques():
     global contador
     contador += 1
-    label_contador.config(text=f"Clique no botão: {contador} :D")  
+    print ("Voce clicou: ",contador)
+    if contador == 10:
+     label_contador.config(text=f"davi voce é um bobão")
+     quit
 root = Tk()
 contador = 0
-frm = ttk.Frame(root, padding=600)
+frm = ttk.Frame(root, padding=200)
 frm.grid()
 label_contador = ttk.Label(frm, text="Clique no botão: 0 :D", font=("Arial", 60))
 label_contador.grid(column=0, row=0)
 botao_contador = ttk.Button(frm, text="Botão clicavel", command=contar_cliques)
 botao_contador.grid(column=0, row=1)
-root.mainloop()
+root.mainloop() 
