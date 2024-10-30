7cd6090c904f77edc37c8155876f1fcd18586b88
alekus2 <alexvinicorreia76@gmail.com>
2024-10-30 12:18:54 -0300

    oie

--- teste.py
+++ teste.py
@@ -1,36 +1,47 @@
+import csv
 
-num=0
+num = 0
+
 def inc():
     global num
     num += 1
     return num
 
-def lista_de_talhoes(parcela_nova):
-    talhoes={talhao : parcela_nova}
-    x = int(input("Insira a quantidade de talhões que deseja alterar: "))
-    for _ in range(x):
-        talhao = inc()  
-        if talhao not in talhoes:
-            talhoes[talhao] = 1
-        else:
-            talhoes[talhao] += 1
-   
-   
-    print(talhoes)
+def carregar_talhoes(arquivo):
+    talhoes = {}
+    try:
+        with open(arquivo, mode='r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                talhao = int(row[0])
+                parcelas_talho = int(row[1])
+                talhoes[talhao] = parcelas_talho
+    except FileNotFoundError:
+        print("Arquivo não encontrado. Verifique o nome do arquivo.")
+    except ValueError:
+        print("Erro ao converter dados do CSV. Verifique o formato.")
+    return talhoes
 
-def contagem():
-    parcela = int(input("Insira a quantidade de parcelas: "))
+def contagem(talhoes):
     global parcela_nova
-    if parcela < 3 or parcela == 2:
-        return parcela
-    else:
-        try:
-            if parcela % 2 == 0:
-                parcela_nova=parcela/2
+    for talhao, parcelas_talho in talhoes.items():
+        if parcelas_talho < 3:
+            parcela_nova = parcelas_talho
+        else:
+            if parcelas_talho % 2 == 0:
+                parcela_nova = parcelas_talho // 2
             else:
-                parcela_nova=(parcela+1)/2
-        except ValueError:
-            print("Entrada inválida.")
-            return 0
-parcela_nova = contagem()
-lista_de_talhoes(parcela_nova)
+                parcela_nova = (parcelas_talho + 1) // 2
+        talhoes[talhao] = parcela_nova
+    print("Talhões e suas parcelas:", talhoes)
+
+def main():
+    arquivo = 'talhoes.csv' 
+    talhoes = carregar_talhoes(arquivo)
+    if talhoes:
+        contagem(talhoes)
+    else:
+        print("Nenhum talhão carregado.")
+
+main()
