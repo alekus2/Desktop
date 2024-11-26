841543f6e721b2bc55d937d5c219aa590a742e22
alekus2 <alexvinicorreia76@gmail.com>
2024-11-26 12:42:05 -0300

    -refazendo script

--- MS florestal/Scrip Script confecção de gráficos, tabelas e pptx.py
+++ MS florestal/Scrip Script confecção de gráficos, tabelas e pptx.py
@@ -1,4 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-x = pd.read_excel (G:\aleks\Atividades-SENAC-AGOST\MS florestal)
+x = pd.read_excel (r"G:\aleks\Atividades-SENAC-AGOST\MS florestal\exemplo excel.xlsx")
+plt.pie(x['Número'],labels=x['Nome'],autopct=None)
+plt.show()
