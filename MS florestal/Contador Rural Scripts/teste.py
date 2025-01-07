a1fbb762cddad117a6858a15f07b121c730a91fe
alekus2 <alekviniciuscorreia@gmail.com>
2025-01-07 09:30:50 -0300

    -sal

--- MS florestal/Contador Rural Scripts/teste.py
+++ MS florestal/Contador Rural Scripts/teste.py
@@ -5,27 +5,24 @@ import datetime
 from arcgis.gis import GIS
 import pandas as pd
 
-
 portalURL = r'https://gissp.bracell.com/portal/'
 username = "Qualidade_SP"
 password = "Qualidade@21"
 survey_item_id = "35572da04c8f4193a518c888f20cda75"
-save_path = r'F:\Qualidade_Florestal\02- MATO GROSSO DO SUL\11- Administrativo Qualidade MS\00- Colaboradores\17 - Alex Vinicius'
+save_path = r'F:\Qualidade_Florestal\02- MATO GROSSO DO SUL\11- Administrativo Qualidade MS\00- Colaboradores\17 - Alex Vinicius\Pasta de controle de mudas laudo'
 keep_org_item = False
 store_csv_w_attachments = True
 
 gis = GIS(portalURL, username, password)
 survey_by_id = gis.content.get(survey_item_id)
 
-
-rel_fs = survey_by_id.related_items('Survey2Service', 'forward')[0]
-item_excel = rel_fs.export(title=survey_by_id.title, export_format='Excel')
+rel_fs = survey_by_id.read_excel('Survey2Service', 'forward')[0]
+item_excel = rel_fs.export(title=survey_by_id, export_format='Excel')
 item_excel.download(save_path=save_path)
 if not bool(keep_org_item):
     item_excel.delete(force=True)
 
-
-url_base_survey = r'F:\Planejamento_e_Controle\Controles Administrativos\1. MS_MSFC FLORESTAL\05 - Compartilhada\12 - Qualidade\00 - Arquivos Survey\QLD_laudo_de_qualidade_de_mudas_em_expedicao.xlsx'
+url_base_survey = r'F:"F:\Qualidade_Florestal\02- MATO GROSSO DO SUL\11- Administrativo Qualidade MS\00- Colaboradores\17 - Alex Vinicius\Pasta de controle de mudas laudo\QLD_laudo_de_qualidade_de_mudas_em_expedicao.xlsx"'
 base_survey = pd.read_excel(url_base_survey)
 
 layers = rel_fs.layers + rel_fs.tables
